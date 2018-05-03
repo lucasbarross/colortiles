0562bcdb034811e704bce3adaecf0cd77d5f33ad
Lucas Barros <lbam@cin.ufpe.br>
2018-05-03 14:22:19 -0300

    Import project from Unity Cloud

---
+++ Assets/Scripts/ColorPacks/ColorPack.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPack : ScriptableObject {
+    public float bonus;
+    public Color[] colors;
+}
